80ef457ef01156d1fce47862c564e70c4789f023
Zachary Blackwood <zachary.blackwood@spensatech.com>
2018-07-17 21:17:24 -0400

    Added AsyncStorage to cache the schedule

--- App.js
+++ App.js
@@ -1,76 +1,102 @@
 import React from 'react';
 import Moment from 'moment';
-import { FlatList, SectionList, ScrollView, ActivityIndicator, Text, View  } from 'react-native';
+import { AsyncStorage, SectionList, ScrollView, ActivityIndicator, Text, View } from 'react-native';
 
 export default class FetchExample extends React.Component {
 
-  constructor(props){
-    super(props);
-    this.state = {isLoading: true}
-  }
-
-  componentDidMount(){
-    let url = 'https://covfamikoiregistration.com/registration/schedule/';
-    return fetch(url)
-      .then(response => response.json())
-      .then(responseJson => {
-        this.setState({
-          isLoading: false,
-          schedule: responseJson,
-        }, function(){
-
-        });
-
-      })
-      .catch((error) =>{
-        console.error(error);
-      });
-  }
-
-
-
-  render(){
-
-    if (this.state.isLoading){
-      return(
-        <View style={{flex: 1, padding: 50}}>
-          <ActivityIndicator/>
-        </View>
-      )
+    constructor(props){
+        super(props);
+        this.state = {isLoading: true};
     }
 
-    Moment.locale('en');
-
-    let days = this.state.schedule.map(item => Moment(item.start).format('YYYY-MM-DD'));
-    days = [...new Set(days)];  // unique
-
-
-    let sections = [];
-    for (let i = 0; i < days.length; i++) {
-        let day = days[i];
-        let data = this.state.schedule.filter(item => Moment(item.start).format('YYYY-MM-DD') == day);
-        sections.push({
-            title: Moment(day).format("dddd, MMM Do"),
-            data,
-        });
+    fetchFromServer() {
+        let url = 'https://covfamikoiregistration.com/registration/schedule/';
+        return fetch(url)
+            .then(response => response.json())
+            .then(responseJson => {
+                this.setState({
+                    isLoading: false,
+                    schedule: responseJson,
+                }, async function() {
+                    try {
+                        await AsyncStorage.setItem('schedule', JSON.stringify(responseJson));
+                    } catch (error) {
+                        console.log(error);
+                    }
+                });
+            })
+            .catch(error => {
+                console.error(error);
+            });
     }
 
-    return (
-        <ScrollView>
-            <SectionList
-                style={{padding: 30}}
-                renderItem={({item, index, section}) => (
-                    <Text key={index}>{item.title}, {item.full_location}, {Moment(item.start).format('h:mm A')} - {Moment(item.end).format('h:mm A')}</Text>
-                )}
-                renderSectionHeader={
-                    ({section: {title}}) => (
-                        <Text style={{fontWeight: 'bold'}}>{title}</Text>
-                    )
-                }
-                sections={sections}
-                keyExtractor={(item, index) => item + index}
-            />
-        </ScrollView>
-    );
-  }
+    async retrieveData() {
+        try {
+            console.log("ATTEMPTING TO RETRIEVE FROM LOCAL STORAGE");
+            let schedule = await AsyncStorage.getItem('schedule');
+            schedule = JSON.parse(schedule);
+            if (schedule !== null) {
+                console.log("SCHEDULE IN LOCAL STORAGE NOT NULL");
+                return this.setState({
+                    isLoading: false,
+                    schedule,
+                });
+            }
+            console.log("SCHEDULE WAS NULL");
+            return this.fetchFromServer();
+        } catch (error) {
+            // Error retrieving data
+            console.log("ATTEMPTING TO RETRIEVE FROM LOCAL STORAGE FAILED");
+            console.log(error);
+            return this.fetchFromServer();
+        }
+    }
+
+    componentDidMount() {
+        this.retrieveData();
+    }
+
+
+    render() {
+        if (this.state.isLoading){
+            return(
+                <View style={{flex: 1, padding: 50}}>
+                    <ActivityIndicator/>
+                </View>
+            )
+        }
+
+        Moment.locale('en');
+
+        let days = this.state.schedule.map(item => Moment(item.start).format('YYYY-MM-DD'));
+        days = [...new Set(days)];  // unique
+
+        let sections = [];
+        for (let i = 0; i < days.length; i++) {
+            let day = days[i];
+            let data = this.state.schedule.filter(item => Moment(item.start).format('YYYY-MM-DD') == day);
+            sections.push({
+                title: Moment(day).format("dddd, MMM Do"),
+                data,
+            });
+        }
+
+        return (
+            <ScrollView>
+                <SectionList
+                    style={{padding: 30}}
+                    renderItem={({item, index, section}) => (
+                        <Text key={index}>{item.title}, {item.full_location}, {Moment(item.start).format('h:mm A')} - {Moment(item.end).format('h:mm A')}</Text>
+                    )}
+                    renderSectionHeader={
+                        ({section: {title}}) => (
+                            <Text style={{fontWeight: 'bold'}}>{title}</Text>
+                        )
+                    }
+                    sections={sections}
+                    keyExtractor={(item, index) => item + index}
+                />
+            </ScrollView>
+        );
+    }
 }
