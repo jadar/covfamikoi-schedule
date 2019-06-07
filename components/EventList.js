7f9c2c4a493271eba93cf3dcc862094605fae397
Jacob Rhoda <jacob@jadar.net>
2019-06-06 23:58:14 -0400

    Styling

--- components/EventList.js
+++ components/EventList.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import React from 'react';
-import { Text, View, SectionList } from 'react-native';
+import { Text, View, SectionList, StyleSheet } from 'react-native';
 import Event from './Event';
 import { RectButton, NativeViewGestureHandler } from 'react-native-gesture-handler';
 // import { withNavigation } from 'react-navigation';
@@ -13,15 +13,14 @@ export default class EventList extends React.Component {
 
     render() {
         return (
-            <NativeViewGestureHandler>
-                <SectionList
+            <SectionList
                 renderItem={this.renderItem}
                 renderSectionHeader={this.renderSectionHeader}
+                SectionSeparatorComponent={Separator}
                 sections={this.props.sections}
                 keyExtractor={(item, index) => item + index}
                 style={{ backgroundColor: '#fff' }}
-                />
-            </NativeViewGestureHandler>
+            />
         );
     }
 
@@ -33,9 +32,34 @@ export default class EventList extends React.Component {
 
     renderSectionHeader({ section: { title } }) {
         return (
-            <Text style={{fontWeight: 'bold'}}>{title}</Text>
-            );
+            <View style={styles.sectionHeader}>
+                <Text style={styles.sectionHeaderTitle}>{title.toUpperCase()}</Text>
+            </View>
+        );
     }
 }
 
+const Separator = () => (
+    <View style={styles.sectionSeparator} />
+);
+
 // export default withNavigation(EventList);
+
+const styles = StyleSheet.create({
+    sectionHeader: {
+        backgroundColor: "#f6f6f6"
+    },
+    sectionHeaderTitle: {
+        fontSize: 12,
+        fontWeight: "600",
+        color: "#404040",
+        paddingTop: 14,
+        paddingLeft: 16, 
+        paddingRight: 16,
+        paddingBottom: 6
+    },
+    sectionSeparator: {
+        height: 1,
+        backgroundColor: '#d7d7d7',
+    }
+});
